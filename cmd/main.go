6277ae2ca981a65409b88dee07bde1b833b4c419
Arngrimur Bjarnason <arngrimur.bjarnason@zimpler.com>
2025-03-16 18:09:22 +0100

    [main] print help

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,13 @@ func main() {
 	image, err := command.GetImage(os.Args[1:])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
+		printHelp()
 		os.Exit(1)
 	}
 	fmt.Fprint(os.Stdout, image)
+}
+
+func printHelp() {
+	fmt.Println("Usage: randomfile <directory>")
+	fmt.Println("Prints the path to a random file in the directory")
 }
